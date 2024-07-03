392368c2260cdb72c5b801844fdebc9872ded155
Melissa Silva <4553576154@estudante.sed.sed.gov.br>
2024-07-02 21:41:07 -0300

    Criando API de piadas e Google Tradutor

---
+++ src/app/pages/porfolio/projetos/projetos.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-projetos',
+  templateUrl: './projetos.component.html',
+  styleUrl: './projetos.component.scss'
+})
+export class ProjetosComponent {
+
+constructor(
+
+  private roteador:Router
+){
+
+}
+
+abreProjeto(rota:string){
+
+  console.log(rota);
+   this.roteador.navigate([rota])
+
+}
+
+
+
+
+
+
+
+}
